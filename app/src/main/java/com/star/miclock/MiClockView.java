d41e4f98ed388bcc686f7241a86020f345632ecb
crystalDf <chendong333@gmail.com>
2017-03-06 23:39:10 +0800

    MiClock

--- app/src/main/java/com/star/miclock/MiClockView.java
+++ app/src/main/java/com/star/miclock/MiClockView.java
@@ -11,7 +11,9 @@ import android.view.View;
 public class MiClockView extends View {
 
     private static final int CIRCLE_STROKE_WIDTH = 2;
-    private static final int DEFAULT_SIZE = 800;
+    private static final int DEFAULT_MEASURED_DIMENSION = 800;
+
+    private static final float PADDING_RATIO = 0.12f;
 
     private int mBackgroundColor;
     private int mLightColor;
@@ -28,6 +30,8 @@ public class MiClockView extends View {
     private Paint mTextPaint;
 
     private Paint mCirclePaint;
+
+    private int mRadius;
 
 
     public MiClockView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -61,11 +65,11 @@ public class MiClockView extends View {
 
         mScaleLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mScaleLinePaint.setStyle(Paint.Style.STROKE);
-        mScaleLinePaint.setColor(mBackgroundColor);
+        mScaleLinePaint.setColor(mDarkColor);
 
         mScaleArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mScaleArcPaint.setStyle(Paint.Style.STROKE);
-        mScaleArcPaint.setColor(mBackgroundColor);
+        mScaleArcPaint.setColor(mDarkColor);
 
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setStyle(Paint.Style.FILL);
@@ -80,8 +84,8 @@ public class MiClockView extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(getMeasuredDimension(DEFAULT_SIZE, widthMeasureSpec),
-                getMeasuredDimension(DEFAULT_SIZE, heightMeasureSpec));
+        setMeasuredDimension(getMeasuredDimension(DEFAULT_MEASURED_DIMENSION, widthMeasureSpec),
+                getMeasuredDimension(DEFAULT_MEASURED_DIMENSION, heightMeasureSpec));
     }
 
     private int getMeasuredDimension(int size, int measureSpec) {
@@ -104,4 +108,11 @@ public class MiClockView extends View {
         return result;
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mRadius = Math.min(w - getPaddingLeft() - getPaddingRight(),
+                h - getPaddingTop() - getPaddingBottom()) / 2;
+    }
 }
